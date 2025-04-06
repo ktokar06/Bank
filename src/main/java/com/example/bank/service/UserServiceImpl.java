e9713eae5ee04265166c34b73d8ce5d02313d81a
k <ktokar06@mail.ru>
2025-04-07 02:32:06 +0300

    Update Commit

--- src/main/java/com/example/bank/service/UserServiceImpl.java
+++ src/main/java/com/example/bank/service/UserServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -51,7 +52,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional(readOnly = true)
     public List<UserDto> getAllUsers() {
-        List<User> users = userRepository.findAll();
+        List<User> users = new ArrayList<>(userRepository.findAll());
 
         return users.stream()
                 .map(user -> {
